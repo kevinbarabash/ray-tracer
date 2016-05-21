ae72ebb0db3ba160889a8a6a954d0808102dfa08
Kevin Barabash <kevinb7@gmail.com>
2016-05-20 22:39:18 -0700

    variations on metaball blending

--- metaballs/metaballs.js
+++ metaballs/metaballs.js
@@ -23,19 +23,52 @@ console.log(image.data);
 for (var y = 0; y < 512; y++) {
     for (var x = 0; x < 512; x++) {
         const offset = 4 * (512 * y + x);
+        const p = 2;
+        const values = [];
 
-        const value = balls.reduce((result, ball) => {
+        balls.reduce((result, ball) => {
             const dx = ball.x - x;
             const dy = ball.y - y;
-            return result + ball.r * ball.r / (dx * dx + dy * dy);
+            const value = Math.pow(Math.abs(ball.r), p) / (Math.pow(Math.abs(dx), p) + Math.pow(Math.abs(dy), p));
+            values.push(value);
+            return result + value;
         }, 0);
 
-        if (value >= 1) {
-            image.data[offset + 2] = ((value - 1) * 255) | 0;
-        }
+        let r = values[0];
+        let b = values[1];
+
+        image.data[offset] = (Math.pow(r, 1) * 255) | 0;
+        image.data[offset + 2] = (Math.pow(b, 1) * 255) | 0;
+
         image.data[offset + 3] = 255;
     }
 }
+
+// for (var y = 0; y < 512; y++) {
+//     for (var x = 0; x < 512; x++) {
+//         const offset = 4 * (512 * y + x);
+//         const p = 2;
+//         const values = [];
+//
+//         const value = balls.reduce((result, ball) => {
+//             const dx = ball.x - x;
+//             const dy = ball.y - y;
+//             const value = Math.pow(Math.abs(ball.r), p) / (Math.pow(Math.abs(dx), p) + Math.pow(Math.abs(dy), p));
+//             values.push(value);
+//             return result + value;
+//         }, 0);
+//
+//         if (value >= 1) {
+//             if (values[0] >= values[1]) {
+//                 image.data[offset] = 255;
+//             } else {
+//                 image.data[offset + 2] = 255;
+//             }
+//         }
+//
+//         image.data[offset + 3] = 255;
+//     }
+// }
 
 console.log(image.data);
 
