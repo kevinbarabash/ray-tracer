bb0610ac04ac0a71a8425d28bcb96e84ae1fb035
Kevin Barabash <kevinb7@gmail.com>
2016-05-30 17:27:33 -0700

    adds object wrappers for buffers, attributes, and shaders (programs)

--- render_to_texture/rtt.js
+++ render_to_texture/rtt.js
@@ -1,16 +1,7 @@
 const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl');
 
-const simple = {
-    uniforms: {},
-    attributes: {},
-    buffers: {},
-};
-
-simple.program = createProgram('simple');
-simple.uniforms.projMatrix = gl.getUniformLocation(simple.program, 'projMatrix');
-simple.attributes.pos = gl.getAttribLocation(simple.program, 'pos');
-simple.attributes.color = gl.getAttribLocation(simple.program, 'color');
+const simple = createProgram('simple');
 simple.buffers.pos = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, 10, 0, 10, 10, 0, 10]), gl.STATIC_DRAW);
 simple.buffers.color = createBuffer(gl.ARRAY_BUFFER, new Float32Array([
     0., 0., 0.,
@@ -21,17 +12,7 @@ simple.buffers.color = createBuffer(gl.ARRAY_BUFFER, new Float32Array([
 simple.buffers.elements = createBuffer(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 3]), gl.STATIC_DRAW);
 
 
-const rtt = {
-    uniforms: {},
-    attributes: {},
-    buffers: {},
-};
-
-rtt.program = createProgram('texture');
-rtt.uniforms.projMatrix = gl.getUniformLocation(rtt.program, 'projMatrix');
-rtt.uniforms.uSampler = gl.getUniformLocation(rtt.program, 'uSampler');
-rtt.attributes.pos = gl.getAttribLocation(rtt.program, 'pos');
-rtt.attributes.uv = gl.getAttribLocation(rtt.program, 'uv');
+const rtt = createProgram('texture');
 rtt.buffers.pos = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, 300, 0, 300, 300, 0, 300]), gl.STATIC_DRAW);
 rtt.buffers.uv = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, 1, 0, 1, 1, 0, 1]), gl.STATIC_DRAW);
 rtt.buffers.elements = createBuffer(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 3]), gl.STATIC_DRAW);
@@ -58,20 +39,18 @@ gl.clear(gl.COLOR_BUFFER_BIT);
 gl.viewport(0, 0, 640, 480);
 
 
-gl.useProgram(simple.program);
-gl.enableVertexAttribArray(simple.attributes.pos);
-gl.enableVertexAttribArray(simple.attributes.color);
+simple.useProgram();
 
 const projMatrix = ortho([], 0, 640, 0, 480, 1, -1);    // near z is positive
 gl.uniformMatrix4fv(simple.uniforms.projMatrix, false, projMatrix);
 
-gl.bindBuffer(gl.ARRAY_BUFFER, simple.buffers.pos);
-gl.vertexAttribPointer(simple.attributes.pos, 2, gl.FLOAT, false, 0, 0);
+simple.buffers.pos.bind();
+simple.attributes.pos.pointer(2, gl.FLOAT, false, 0, 0);
 
-gl.bindBuffer(gl.ARRAY_BUFFER, simple.buffers.color);
-gl.vertexAttribPointer(simple.attributes.color, 3, gl.FLOAT, false, 0, 0);
+simple.buffers.color.bind();
+simple.attributes.color.pointer(3, gl.FLOAT, false, 0, 0);
 
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, simple.buffers.elements);
+simple.buffers.elements.bind();
 gl.drawElements(gl.TRIANGLE_FAN, 4, gl.UNSIGNED_SHORT, 0);
 
 gl.flush();
@@ -80,9 +59,7 @@ gl.flush();
 
 gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 
-gl.useProgram(rtt.program);
-gl.enableVertexAttribArray(rtt.attributes.pos);
-gl.enableVertexAttribArray(rtt.attributes.uv);
+rtt.useProgram();
 
 gl.uniformMatrix4fv(rtt.uniforms.projMatrix, false, projMatrix);
 
@@ -90,13 +67,13 @@ gl.activeTexture(gl.TEXTURE0);
 gl.bindTexture(gl.TEXTURE_2D, tex);
 gl.uniform1i(rtt.uniforms.uSampler, 0);
 
-gl.bindBuffer(gl.ARRAY_BUFFER, rtt.buffers.pos);
-gl.vertexAttribPointer(rtt.attributes.pos, 2, gl.FLOAT, false, 0, 0);
+rtt.buffers.pos.bind();
+rtt.attributes.pos.pointer(2, gl.FLOAT, false, 0, 0);
 
-gl.bindBuffer(gl.ARRAY_BUFFER, rtt.buffers.uv);
-gl.vertexAttribPointer(rtt.attributes.uv, 2, gl.FLOAT, false, 0, 0);
+rtt.buffers.uv.bind();
+rtt.attributes.uv.pointer(2, gl.FLOAT, false, 0, 0);
 
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, rtt.buffers.elements);
+rtt.buffers.elements.bind();
 gl.drawElements(gl.TRIANGLE_FAN, 4, gl.UNSIGNED_SHORT, 0);
 
 gl.flush();
