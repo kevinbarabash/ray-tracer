c0605a08d70db6656f0622558e535bb4a1660dc7
Kevin Barabash <kevinb7@gmail.com>
2016-05-30 17:36:12 -0700

    adds random function to rtt

--- render_to_texture/simple/frag.glsl
+++ render_to_texture/simple/frag.glsl
@@ -1,7 +1,19 @@
-precision mediump float;
+precision highp  float;
 
 varying vec3 vColor;
+varying vec2 vPos;
+
+float rand(vec2 co)
+{
+    float a = 12.9898;
+    float b = 78.233;
+    float c = 43758.5453;
+    float dt = dot(co.xy ,vec2(a,b));
+    float sn = mod(dt,3.14);
+    return fract(sin(sn) * c);
+}
 
 void main() {
-    gl_FragColor = vec4(vColor, 1.);
+    float b = rand(vPos);
+    gl_FragColor = vec4(vColor.r, vColor.g, b, 1.);
 }
--- render_to_texture/simple/vert.glsl
+++ render_to_texture/simple/vert.glsl
@@ -6,8 +6,10 @@ attribute vec3 color;
 uniform mat4 projMatrix;
 
 varying vec3 vColor;
+varying vec2 vPos;
 
 void main() {
     gl_Position = projMatrix * vec4(pos, 0., 1.);
     vColor = color;
+    vPos = pos;
 }
