dbe12c0847c60e7ae959c0118c3518cd38276cf7
Kevin Barabash <kevinb7@gmail.com>
2016-06-03 08:59:31 -0700

    updates metaballs example to use new helper functions

--- metaballs/gl_metaballs.js
+++ metaballs/gl_metaballs.js
@@ -6,41 +6,14 @@ const gl = canvas.getContext('experimental-webgl');
 
 const program = createProgram('metaballs');
 
-
-const attributes = {};
-attributes.textureCoords = gl.getAttribLocation(program, 'texCoords2');
-attributes.position = gl.getAttribLocation(program, 'position');
-gl.enableVertexAttribArray(attributes.textureCoords);
-gl.enableVertexAttribArray(attributes.position);
-
-const uniforms = {};
-uniforms.projMatrix = gl.getUniformLocation(program, 'projMatrix');
-uniforms.t = gl.getUniformLocation(program, 't');
-uniforms.mouse = gl.getUniformLocation(program, 'mouse');
-console.log(uniforms);
-
-
-const buffers = {};
-
 const w = 512;
 const h = 512;
 
 let mouse = [0, 0];
 
-// buffers for attributes
-buffers.position = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, w, 0, w, h, 0, h]), gl.STATIC_DRAW);
-
-buffers.textureCoords = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoords);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, w, 0, w, h, 0, h]), gl.STATIC_DRAW);
-
-
-// buffer for triangles
-buffers.faces = gl.createBuffer();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.faces);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 3]), gl.STATIC_DRAW);
+program.buffers.position = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, w, 0, w, h, 0, h]), gl.STATIC_DRAW);
+program.buffers.texCoords2 = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, w, 0, w, h, 0, h]), gl.STATIC_DRAW);
+program.buffers.elements = createBuffer(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 3]), gl.STATIC_DRAW);
 
 
 gl.clearColor(0.5, 0.5, 0.5, 1);
@@ -50,21 +23,21 @@ let t = 0;
 
 const draw = () => {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.useProgram(program);
+    program.useProgram();
 
     const projMatrix = ortho([], 0, w, 0, h, -1, 1);
-    gl.uniformMatrix4fv(uniforms.projMatrix, false, projMatrix);
-    gl.uniform1f(uniforms.t, t++);
-    gl.uniform2fv(uniforms.mouse, mouse);
+    gl.uniformMatrix4fv(program.uniforms.projMatrix, false, projMatrix);
+    gl.uniform1f(program.uniforms.t, t++);
+    gl.uniform2fv(program.uniforms.mouse, mouse);
 
     // set up vertex attributes before drawing
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-    gl.vertexAttribPointer(attributes.position, 2, gl.FLOAT, false, 2 * 4, 0);
+    program.buffers.position.bind();
+    program.attributes.position.pointer(2, gl.FLOAT, false, 0, 0);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoords);
-    gl.vertexAttribPointer(attributes.textureCoords, 2, gl.FLOAT, false, 2 * 4, 0);
+    program.buffers.texCoords2.bind();
+    program.attributes.texCoords2.pointer(2, gl.FLOAT, false, 0, 0);
 
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.faces);
+    program.buffers.elements.bind();
     gl.drawElements(gl.TRIANGLE_FAN, 4, gl.UNSIGNED_SHORT, 0);
 
     gl.flush();
@@ -93,11 +66,3 @@ document.addEventListener('mouseup', (e) => {
         mouse = [e.pageX, h - e.pageY];
     }
 });
-
-
-console.log('buffers');
-console.log(buffers);
-
-console.log('attributes');
-console.log(attributes);
-
