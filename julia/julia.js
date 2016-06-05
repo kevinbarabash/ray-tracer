84bd215dbf1d1658ee33afcbede7b8d66fd47496
Kevin Barabash <kevinb7@gmail.com>
2016-06-04 21:27:16 -0700

    adds panning to julia fractal

--- julia/julia.js
+++ julia/julia.js
@@ -1,23 +1,40 @@
 document.body.style.margin = 0;
 
-const canvas = document.querySelector('canvas');
-const gl = canvas.getContext('experimental-webgl');
+const canvas = document.createElement('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+document.body.appendChild(canvas);
+
+const gl = canvas.getContext('experimental-webgl');
 
 const program = createProgram('julia');
 
-const w = 768;
-const h = 768;
+const w = window.innerWidth;
+const h = window.innerHeight;
 
 let mouse = [0, 0];
+let left, right, bottom, _top;
+let dw, dh;
 
-const left = -1.5;
-const right = 1.5;
-const bottom = -1.5;
-const _top = 1.5;
+const size = 3;
+
+if (canvas.width > canvas.height) {
+    dh = 3 / canvas.height;
+    dw = dh;
+
+    left = -size / 2 * canvas.width / canvas.height;
+    right = size / 2 * canvas.width / canvas.height;
+    bottom = -size / 2;
+    _top = size / 2;
+} else {
+    // TODO implement this
+}
+
+const bounds = new Float32Array([left, bottom, right, bottom, right, _top, left, _top]);
 
 program.buffers.pos = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, w, 0, w, h, 0, h]), gl.STATIC_DRAW);
-program.buffers.uv = createBuffer(gl.ARRAY_BUFFER, new Float32Array([left, bottom, right, bottom, right, _top, left, _top]), gl.STATIC_DRAW);
+program.buffers.uv = createBuffer(gl.ARRAY_BUFFER, bounds, gl.STATIC_DRAW);
 program.buffers.elements = createBuffer(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 3]), gl.STATIC_DRAW);
 
 
@@ -38,6 +55,8 @@ const draw = () => {
     program.attributes.pos.pointer(2, gl.FLOAT, false, 0, 0);
 
     program.buffers.uv.bind();
+
+    program.buffers.uv.update(bounds);
     program.attributes.uv.pointer(2, gl.FLOAT, false, 0, 0);
 
     program.buffers.elements.bind();
@@ -45,23 +64,33 @@ const draw = () => {
 
     gl.flush();
 
-    // requestAnimationFrame(draw);
+    requestAnimationFrame(draw);
 };
 
-draw();
-
-// requestAnimationFrame(draw);
+requestAnimationFrame(draw);
 
 let drag = false;
+let lastMouse;
 
 document.addEventListener('mousedown', (e) => {
-    mouse = [e.pageX, h - e.pageY];
+    lastMouse = [e.pageX, h - e.pageY];
     drag = true;
 });
 
 document.addEventListener('mousemove', (e) => {
     if (drag) {
         mouse = [e.pageX, h - e.pageY];
+        const dx = mouse[0] - lastMouse[0];
+        const dy = mouse[1] - lastMouse[1];
+        bounds[0] -= dx * dw;
+        bounds[2] -= dx * dw;
+        bounds[4] -= dx * dw;
+        bounds[6] -= dx * dw;
+        bounds[1] -= dy * dh;
+        bounds[3] -= dy * dh;
+        bounds[5] -= dy * dh;
+        bounds[7] -= dy * dh;
+        lastMouse = mouse;
     }
 });
 
@@ -69,5 +98,16 @@ document.addEventListener('mouseup', (e) => {
     if (drag) {
         drag = false;
         mouse = [e.pageX, h - e.pageY];
+        const dx = mouse[0] - lastMouse[0];
+        const dy = mouse[1] - lastMouse[1];
+        bounds[0] -= dx * dw;
+        bounds[2] -= dx * dw;
+        bounds[4] -= dx * dw;
+        bounds[6] -= dx * dw;
+        bounds[1] -= dy * dh;
+        bounds[3] -= dy * dh;
+        bounds[5] -= dy * dh;
+        bounds[7] -= dy * dh;
+        lastMouse = mouse;
     }
 });
