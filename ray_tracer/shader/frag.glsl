1a37f4cc84c91c53f81ed81a30e87346f5bf9b69
Kevin Barabash <kevinb7@gmail.com>
2016-06-21 21:22:51 -0700

    adds multipl spheres, shading, and multisampling antiliasing

--- ray_tracer/shader/frag.glsl
+++ ray_tracer/shader/frag.glsl
@@ -39,20 +39,58 @@ void raySphereIntersection(in vec3 O, in vec3 D, in Sphere sphere, out int num_s
 }
 
 void main() {
-    Sphere sphere1 = Sphere(vec3(0., 0., 0.), 256., vec3(1., 0., 0.));
+    vec3 sun = normalize(vec3(1., 1., 1.));
 
-    float z = 512.;
-    vec3 D = vec3(0., 0., -1.);
-    vec3 O = vec3(vPosition, z);
+    Sphere spheres[2];
 
-    int num_solutions;
-    float solutions[2];
+    spheres[0] = Sphere(vec3(256., 256., 0.), 128., vec3(0., 1., 1.));
+    spheres[1] = Sphere(vec3(400., 400., 0.), 64., vec3(1., 0., 0.));
 
-    raySphereIntersection(O, D, sphere1, num_solutions, solutions);
+    // antialising offsets
+    vec2 offsets[4];
+    offsets[0] = vec2(0.25, 0.25);
+    offsets[1] = vec2(0.75, 0.25);
+    offsets[2] = vec2(0.75, 0.75);
+    offsets[3] = vec2(0.25, 0.75);
 
-    if (num_solutions != 0) {
-        gl_FragColor = vec4(1., 0., 1., 1.);
-    } else {
-        gl_FragColor = vec4(0., 0., 0., 1.);
+    vec3 avg_color = vec3(0., 0., 0.);
+
+    for (int k = 0; k < 4; k++) {
+        vec3 color = vec3(0.25, 0.25, 0.25);
+        float z_max = -1000.;
+
+        for (int i = 0; i < 2; i++) {
+            Sphere sphere = spheres[i];
+
+            float camera_z = 512.;
+            vec3 D = vec3(0., 0., -1.);
+            vec3 O = vec3(vPosition + offsets[k], camera_z);
+
+            int num_solutions;
+            float solutions[2];
+
+            raySphereIntersection(O, D, sphere, num_solutions, solutions);
+
+            for (int j = 0; j < 2; j++) {
+                if (j >= num_solutions) {
+                    continue;
+                }
+                float t = solutions[j];
+                vec3 hit = O + t * D;
+
+                // z+ is coming out of the screen
+                if (hit.z > z_max) {
+                    z_max = hit.z;
+
+                    vec3 N = normalize(hit - sphere.center);
+                    float shade = dot(sun, N);
+                    color = shade * sphere.color;
+                }
+            }
+        }
+
+        avg_color += color;
     }
+
+    gl_FragColor = vec4(0.25 * avg_color, 1.);
 }
