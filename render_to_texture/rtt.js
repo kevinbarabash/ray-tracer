6858131a2016e885d2bdf5340d6e85848571fc53
Kevin Barabash <kevinb7@gmail.com>
2016-06-26 13:58:58 -0700

    renders brush strokes with better blending

--- render_to_texture/rtt.js
+++ render_to_texture/rtt.js
@@ -2,7 +2,8 @@ const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl');
 
 const simple = createProgram('simple');
-simple.buffers.pos = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, 10, 0, 10, 10, 0, 10]), gl.STATIC_DRAW);
+const size = 300;
+simple.buffers.pos = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, size, 0, size, size, 0, size]), gl.STATIC_DRAW);
 simple.buffers.color = createBuffer(gl.ARRAY_BUFFER, new Float32Array([
     0., 0., 0.,
     1., 0., 0.,
@@ -12,14 +13,10 @@ simple.buffers.color = createBuffer(gl.ARRAY_BUFFER, new Float32Array([
 simple.buffers.elements = createBuffer(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 3]), gl.STATIC_DRAW);
 
 
-const rtt = createProgram('texture');
-rtt.buffers.pos = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, 300, 0, 300, 300, 0, 300]), gl.STATIC_DRAW);
-rtt.buffers.uv = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, 1, 0, 1, 1, 0, 1]), gl.STATIC_DRAW);
-rtt.buffers.elements = createBuffer(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 3]), gl.STATIC_DRAW);
-
+var tex2 = createTexture(gl.TEXTURE_2D, gl.RGBA, size, size, { minFilter: gl.LINEAR, magFilter: gl.NEAREST });
 
 // create an empty texture
-var tex = createTexture(gl.TEXTURE_2D, gl.RGBA, 10, 10, { minFilter: gl.LINEAR, magFilter: gl.NEAREST });
+var tex = createTexture(gl.TEXTURE_2D, gl.RGBA, size, size, { minFilter: gl.LINEAR, magFilter: gl.NEAREST });
 
 // create a fbo and attac the texture to it
 var fb = gl.createFramebuffer();
@@ -36,6 +33,9 @@ simple.useProgram();
 
 const projMatrix = ortho([], 0, 640, 0, 480, 1, -1);    // near z is positive
 gl.uniformMatrix4fv(simple.uniforms.projMatrix, false, projMatrix);
+gl.activeTexture(gl.TEXTURE1);
+tex2.bind();
+gl.uniform1i(simple.uniforms.uSampler, 1);
 
 simple.buffers.pos.bind();
 simple.attributes.pos.pointer(2, gl.FLOAT, false, 0, 0);
@@ -48,6 +48,12 @@ gl.drawElements(gl.TRIANGLE_FAN, 4, gl.UNSIGNED_SHORT, 0);
 
 gl.flush();
 
+
+
+const rtt = createProgram('texture');
+rtt.buffers.pos = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, 300, 0, 300, 300, 0, 300]), gl.STATIC_DRAW);
+rtt.buffers.uv = createBuffer(gl.ARRAY_BUFFER, new Float32Array([0, 0, 1, 0, 1, 1, 0, 1]), gl.STATIC_DRAW);
+rtt.buffers.elements = createBuffer(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 3]), gl.STATIC_DRAW);
 
 
 gl.bindFramebuffer(gl.FRAMEBUFFER, null);
--- render_to_texture/simple/frag.glsl
+++ render_to_texture/simple/frag.glsl
@@ -3,6 +3,11 @@ precision highp  float;
 varying vec3 vColor;
 varying vec2 vPos;
 
+uniform sampler2D uSampler;
+
+#define E 2.718281828459
+#define PI 3.14159
+
 float rand(vec2 co)
 {
     float a = 12.9898;
@@ -14,6 +19,47 @@ float rand(vec2 co)
 }
 
 void main() {
-    float b = rand(vPos);
-    gl_FragColor = vec4(vColor.r, vColor.g, b, 1.);
+    float radius = 50.;
+
+    float a, d;
+    vec2 center;
+
+    vec2 uv = vPos / 100.;
+    vec3 brushColor = vec3(0., 0., 1.);
+    vec3 color = vec3(1., 1., 1.);
+
+    float x = 50.;
+    float y = 50.;
+
+    vec3 exp = vec3(E);
+
+    for (int i = 0; i < 100; i++) {
+        center = vec2(x, y);
+
+        d = distance(center, vPos);
+        a = smoothstep(0., 1., d / radius);
+
+        color = pow(a * pow(color, exp) + (1. - a) * pow(brushColor, exp), 1. / exp);
+
+        x += 5.;
+        y += pow(float(i) / 10., 2.);
+    }
+
+    brushColor = vec3(0., 1., 0.);
+
+    x = 50.;
+    y = 50.;
+    for (int i = 0; i < 100; i++) {
+        center = vec2(x, 300. - y);
+
+        d = distance(center, vPos);
+        a = smoothstep(0., 1., d / radius);
+
+        color = pow(a * pow(color, exp) + (1. - a) * pow(brushColor, exp), 1. / exp);
+
+        x += 5.;
+        y += pow(float(i) / 10., 2.);
+    }
+
+    gl_FragColor = vec4(color, 1.);
 }
