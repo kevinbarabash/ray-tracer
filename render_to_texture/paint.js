c2f1963cdf629e8b8a4327bdaa0c3f690f17267d
Kevin Barabash <kevinb7@gmail.com>
2016-06-29 08:14:18 -0700

    updates paint example to draw lines with a series of equally spaced circles

--- render_to_texture/paint.js
+++ render_to_texture/paint.js
@@ -33,16 +33,47 @@ const randomColor = (a = 1.0) => {
 };
 
 const distance = (p1, p2) => {
-    const dx = p2.x - p1.x;
-    const dy = p2.y - p1.y;
+    const dx = p2[0] - p1[0];
+    const dy = p2[1] - p1[1];
     // TODO: remove square root and check distance squared instead
     return Math.sqrt(dx * dx + dy * dy);
 };
 
-downs.onValue((e) => ctx.fillStyle = randomColor());
-drags.onValue((e) => {
-    const p = { x: event.pageX, y: event.pageY };
-    const d = distance(lastPoint, p);
-    console.log(`d = ${d}`);
-    ctx.fillCircle(e.pageX, e.pageY, 20);
+downs.onValue((e) => {
+    ctx.fillStyle = randomColor();
+    lastPoint = [e.pageX, e.pageY];
 });
+drags.onValue((e) => {
+    const currentPoint = [e.pageX, e.pageY];
+    const d = distance(lastPoint, currentPoint);
+    if (d >= spacing) {
+        lastPoint = line(lastPoint, currentPoint);
+    }
+});
+
+ctx.fillStyle = 'rgba(0, 0, 255, 1.0)';
+
+
+const radius = 10;
+const spacing = 2 * radius;
+
+const line = (start, end) => {
+    const d = distance(end, start);
+
+    const cos = (end[0] - start[0]) / d;
+    const sin = (end[1] - start[1]) / d;
+
+    let p = [...start];
+    let t = 0;
+
+    while (t + spacing < d) {
+        p[0] += spacing * cos;
+        p[1] += spacing * sin;
+        t += spacing;
+        ctx.fillCircle(...p, radius);
+    }
+
+    return p;
+};
+
+line([100, 100], [400, 300]);
