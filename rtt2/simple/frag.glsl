f676d2d1a59b9035216a433d949b9df44f4f2fa2
Kevin Barabash <kevinb7@gmail.com>
2016-07-12 09:31:59 -0700

    transpose xyz2rgb and rgb2xyz matrices

--- rtt2/simple/frag.glsl
+++ rtt2/simple/frag.glsl
@@ -4,21 +4,13 @@ uniform vec3 uColor;
 uniform float uRadius;
 
 mat3 rgb2xyz = mat3(
-    0.4124, 0.3576, 0.1805,
-    0.2126, 0.7152, 0.0722,
-    0.0193, 0.1192, 0.9505
+    0.4124, 0.2126, 0.0193,     // col 1
+    0.3576, 0.7152, 0.1192,     // col 2
+    0.1805, 0.0722, 0.9505      // col 3
 );
 
 void main() {
-    vec2 point = gl_PointCoord;
-    vec2 center = vec2(0.5, 0.5);
-
-    point = 2. * (point - center);
-
-    float a = 1. - length(point);
-    float pxSize = 1. / uRadius;
-
-    a = smoothstep(0.0, 1.0, a);
+    float a = 1. - clamp(length(2. * (gl_PointCoord - vec2(0.5))), 0., 1.);
 
     gl_FragColor = vec4(rgb2xyz * uColor, a);
 }
